7ae34497b3ef8fa1458f79c3ca50bba40a5215ea
Serious-Fin <augustasgri@gmail.com>
2023-08-10 23:01:06 +0300

    made the cards clickable once with changing colors

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,23 @@
-import React from "react";
 import "./Card.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function Card() {
+function Card({ color }) {
   const [isClicked, setIsClicked] = useState(false);
+  const [cardColor, setCardColor] = useState(color);
 
   function handleClick() {
     setIsClicked(true);
+    setCardColor("#b8b8b8");
     console.log("clicked");
   }
+
+  useEffect(() => {}, [cardColor]);
 
   return (
     <div
       className={`card ${isClicked ? "clicked" : ""}`}
       onClick={handleClick}
+      style={{ backgroundColor: cardColor }}
     ></div>
   );
 }
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -2,11 +2,39 @@ import "./board.css";
 import Card from "./Card.jsx";
 import React from "react";
 
+const colors = [
+  "#FF5733",
+  "#C70039",
+  "#900C3F",
+  "#FFC300",
+  "#DAF7A6",
+  "#00CC99",
+  "#8E44AD",
+  "#3498DB",
+  "#2ECC71",
+  "#F39C12",
+  "#16A085",
+  "#E74C3C",
+  "#9B59B6",
+  "#2980B9",
+  "#27AE60",
+  "#E67E22",
+  "#1ABC9C",
+  "#D35400",
+  "#2C3E50",
+  "#3498DB",
+  "#27AE60",
+  "#FF5733",
+  "#1ABC9C",
+];
+
 function Board() {
   const cards = [];
 
   for (let i = 0; i < 25; i++) {
-    cards.push(<Card key={i} />);
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const color = colors[randomIndex];
+    cards.push(<Card key={i} color={color} />);
   }
 
   return <div className="board">{cards}</div>;
